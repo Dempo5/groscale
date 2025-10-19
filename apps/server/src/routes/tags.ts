7faffcfc3b3e8d10079024b6d88006eeada7793f
Dempo5 <eelamb05@gmail.com>
2025-10-19 14:13:06 -0400

    Update tags.ts

--- apps/server/src/routes/tags.ts
+++ apps/server/src/routes/tags.ts
@@ -1,8 +1,7 @@
 // apps/server/src/routes/tags.ts
 import { Router } from "express";
 import bcrypt from "bcryptjs";
-import { prisma } from "../../prisma.js";
-
+import { prisma } from "../db.js";
 const router = Router();
 
 /** Shape returned to client */
