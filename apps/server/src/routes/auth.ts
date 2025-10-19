f64328b787e4f2318d39c6bebcc29d227fe7eab4
Dempo5 <eelamb05@gmail.com>
2025-10-19 03:37:12 -0400

    Update auth.ts

--- apps/server/src/routes/auth.ts
+++ apps/server/src/routes/auth.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import bcrypt from "bcryptjs";
-import { prisma } from "../../prisma.js";                 // unified prisma import
+import { PrismaClient } from "@prisma/client";
 import { signToken, requireAuth, AuthedRequest } from "../middleware/auth.js";
 import { Prisma } from "@prisma/client";
+const prisma = new PrismaClient();
+                 // unified prisma import
 
 const router = Router();
 
