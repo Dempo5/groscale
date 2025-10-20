8450b7e0bd08117412746416949e406b7b3d29dd
Dempo5 <eelamb05@gmail.com>
2025-10-20 14:16:53 -0400

    Update workflows.ts

--- apps/server/src/routes/workflows.ts
+++ apps/server/src/routes/workflows.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
-import { PrismaClient, Prisma } from "@prisma/client";
+import { prisma } from "../../prisma.js";
 
 const prisma = new PrismaClient();
 const router = Router();
