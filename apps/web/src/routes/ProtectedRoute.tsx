c3c972141d2f10d5749f1eca8dc1513421e66ed6
Ethan Lamb <eelamb05@gmail.com>
2026-08-22 11:33:36 -0400

    Delete apps/web/src/routes/ProtectedRoute.tsx

--- apps/web/src/routes/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-// apps/web/src/routes/ProtectedRoute.tsx
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { isAuthed } from "../lib/api";
-
-export default function ProtectedRoute() {
-  const authed = isAuthed();
-  const loc = useLocation();
-  return authed ? <Outlet /> : <Navigate to="/login" replace state={{ from: loc }} />;
-}
